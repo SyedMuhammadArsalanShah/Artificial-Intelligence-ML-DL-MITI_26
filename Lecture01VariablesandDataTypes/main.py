d024362006d92765876c49e83166cef5a69ed046
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2026-03-05 15:51:42 +0500

    CodE lec01

---
+++ Lecture01VariablesandDataTypes/main.py
@@ -0,0 +1,30 @@
+print("Bismillah ")
+
+
+
+# variables and datatypes 
+
+# string 
+
+
+userName="Abdul Jabbar"
+
+# integer
+
+age=27
+
+# float
+height=5.9
+
+
+# boolean 
+
+isStudentEnrolled=True
+
+
+
+# concat 
+print("Student Name "+userName)
+print("Student Age",age)
+print("Student Height ",height)
+print("Student Enrollment Status",isStudentEnrolled)
