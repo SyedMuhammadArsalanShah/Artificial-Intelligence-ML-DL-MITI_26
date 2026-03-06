9976a1141120232eb4118347eebf7c646ad0274e
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2026-03-06 15:50:08 +0500

    code

---
+++ Lecture02UserInputs/L01UserInputs.py
@@ -0,0 +1,10 @@
+
+# name=input("Enter Your Name Here ")
+# email=input("Enter Your Email Here ")
+# contact=input("Enter Your Contact Here ")
+# password=input("Enter Your Password Here ")
+
+
+# print("Account Successfully Created ")
+
+
---
+++ Lecture02UserInputs/L02BasicCalculator.py
@@ -0,0 +1,25 @@
+
+number1= int(input("Enter Your Number 1 Here "))
+number2= int(input("Enter Your Number 2 Here "))
+
+addkaren=number1+number2
+
+subkaren=number1-number2
+
+mulkaren=number1*number2
+
+divkaren=number1/number2
+
+remainderkaren=number1%number2
+
+expkaren=number1**number2
+
+avgkaren=(number1+number2)/2
+
+print("Add hova hai ", addkaren)
+print("Sub hova hai ", subkaren)
+print( "Mul hova hai ",mulkaren)
+print( "Div hova hai ",divkaren)
+print("remainder hova hai ",remainderkaren)
+print( "Exponention hova hai ",expkaren)
+print( "Avg  hova hai ",avgkaren)
