d69784bffbb2e14271b4c8142eeacf6b19ca0331
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2026-03-31 20:45:10 +0500

    Code

---
+++ Lecture07Loops/superMITIMart.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+
+
+numberOfItems=int(input("Enter Your Number Of Items\n"))
+finalBill=0
+for i in range(1,numberOfItems+1):
+    print("Item No",i)
+    itemsPrice=int(input("Enter Your Item Price \n"))
+    finalBill=finalBill+itemsPrice
+    
+print("Your Total Bill is",finalBill )
+
+
+if finalBill <=10000:
+    discount =finalBill*0.2
+    afterDiscBill=finalBill-discount
+    print("After Discount Your Bill Is ", afterDiscBill)
+elif finalBill <=20000:
+    discount =finalBill*0.5
+    afterDiscBill=finalBill-discount
+    print("After Discount Your Bill Is ", afterDiscBill)
+else:
+    discount =finalBill*0.75
+    afterDiscBill=finalBill-discount
+    print("After Discount Your Bill Is ", afterDiscBill)
