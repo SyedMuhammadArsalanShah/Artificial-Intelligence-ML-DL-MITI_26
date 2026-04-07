2ab127d4edf0ed97f7bf25102a76ead424927a0a
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2026-04-07 20:35:32 +0500

    Code

--- Lecture07Loops/MITI_MART.py
+++ Lecture07Loops/MITI_MART.py
@@ -1,8 +1,39 @@
-import streamlit as st
-st.title("Super MITI MART 🛒")
-price_input=st.text_area("Enter Your Product Price by Comma and stop by 0")
-if st.button("Calculate Bill"):
-    item_price=[int(x.strip())for x in price_input.split(",")]
 
+import streamlit as st 
+
+
+st.title("MITI Super Mart 🛒")
+
+
+price_input = st.text_area("Enter Your All product price with comma sepration ")
+
+if st.button("Bill do bhai "):
     
+    item_prices=[int(x.strip()) for x in price_input.split(",")]
+    
+    finalBill=0
+    i=0
+    while i< len(item_prices):
+        if item_prices[i]==00:
+            st.write("Thanks For Shopping In Mart")
+            break;
+        st.write(f"Item No{i+1}")
+        st.write(f"Item Price{item_prices[i]}")
+        finalBill=finalBill+item_prices[i]
+        i=i+1
+    st.write(f"Your Final Bill is {finalBill}")
+    
+    
+    if finalBill <=10000:
+        discount= finalBill*0.20
+    elif finalBill <=20000:
+        discount= finalBill*0.5
+    else:
+        discount=finalBill*0.75
+        
+        
+    afterDiscBill= finalBill-discount
+    st.write(f"After Discount Bill is {afterDiscBill}")
+    
+
     
